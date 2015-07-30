709a6038d560250f2165fa0af9576a2522cf52fd
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-07-30 13:06:13 +0200

    Added gitignore + setting up the structure a bit better

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var app = require('app');  // Module to control application life.
+var nedflixApi = require('nedflix-api');
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
 
 // Report crashes to our server.
 require('crash-reporter').start();
 
+console.log(nedflixApi.getRTL());  
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is GCed.
 var mainWindow = null;
@@ -24,7 +26,7 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadUrl('file://' + __dirname + '/../index.html');
 
   // Open the devtools.
   mainWindow.openDevTools();
